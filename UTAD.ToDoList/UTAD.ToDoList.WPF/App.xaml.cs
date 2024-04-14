651c75248fd607c254555571e18fe45227a501a1
Tiago Silva <tiagoissilva@hotmail.com>
2024-04-14 15:25:55 +0100

    Mudanças gerais do projeto

--- UTAD.ToDoList/UTAD.ToDoList.WPF/App.xaml.cs
+++ UTAD.ToDoList/UTAD.ToDoList.WPF/App.xaml.cs
@@ -11,10 +11,13 @@ namespace UTAD.ToDoList.WPF
     /// </summary>
     public partial class App : Application
     {
+        // propriedades dos modelos
+        public Perfil Perfil { get; set; }
+
+        // propriedades das views
         public MainWindow MainWindow { get; set; }
         public ViewLogin ViewLogin { get; set; }
         public ViewRegisto ViewRegisto { get; set; }
-        // propriedades das views
         
 
         public App()
